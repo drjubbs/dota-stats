88a2ceb917e7bc9f3372f50ead6abb771943373d
Your Name <you@example.com>
2020-09-02 14:44:48 -0400

    Linting and cleanup of compact_db module

--- compact_db.py
+++ compact_db.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
-# Compact SQLite3 into MariaDB
-# 
-# Older verions of this code generated SQLLite3 files instead of using a database, load these in.
-
+"""Compact SQLite3 into MariaDB
+Older verions of this code generated SQLLite3 files instead of using a
+database, load these into MariaDB. File should be mostly unused now.
+"""
 import sqlite3
 import os
-import json
-import pandas as pd
-import datetime as dt
 import re
 import mariadb
 
+BATCH_SIZE=2000
+
 # Merge database files and remove duplicates
-DB_FILES=[]
-for filename in os.listdir("."):    
-    if re.match("^.*db$",filename) is not None:    
-        DB_FILES.append(filename)
-                    
+db_files=[]
+for filename in os.listdir("."):
+    if re.match("^.*db$",filename) is not None:
+        db_files.append(filename)
+
 rows=[]
-for this_file in DB_FILES:
+for this_file in db_files:
     conn=sqlite3.connect(this_file)
     c=conn.cursor()
     c.execute("SELECT match_id, start_time, radiant_heroes, dire_heroes, "\
@@ -35,7 +34,7 @@ print("Removing duplicates: {0}".format(len(rows)))
 conn_maria = mariadb.connect(
     user=os.environ['DOTA_USERNAME'],
     password=os.environ['DOTA_PASSWORD'],
-    host="localhost",
+    host=os.environ['DOTA_HOSTNAME'],
     database=os.environ['DOTA_DATABASE'])
 
 c=conn_maria.cursor()
@@ -43,17 +42,16 @@ c.execute("select count(*) from dota_matches")
 print("Rows before merge: {0} ".format(c.fetchall()[0][0]))
 
 # Batch samples in
-BATCH_SIZE=2000
-for i in range(len(rows)//BATCH_SIZE+1):    
+for i in range(len(rows)//BATCH_SIZE+1):
     begin=i*BATCH_SIZE
     end=min(len(rows),(i+1)*BATCH_SIZE)
     print("{0} to {1}".format(begin,end))
-    stmt="INSERT IGNORE INTO dota_matches (match_id, start_time, "\
+    STMT="INSERT IGNORE INTO dota_matches (match_id, start_time, "\
                   "radiant_heroes, dire_heroes, radiant_win, api_skill, "\
                   "items, gold_spent) values(?, ?, ?, ?, ?, ?, ?, ?)"
-    print(stmt)
-    c.executemany(stmt, list(rows)[begin:end])
-    
+    print(STMT)
+    c.executemany(STMT, list(rows)[begin:end])
+
 conn_maria.commit()
 c.execute("select count(*) from dota_matches")
 print("Rows after merge: {0} ".format(c.fetchall()[0][0]))
