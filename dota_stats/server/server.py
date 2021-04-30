4df31a650572a98fe0eba955f192d314063d39db
Ubuntu <ubuntu@poisonberry.lan>
2021-04-30 18:56:44 +0000

    Webserver beta with new back-end

--- dota_stats/server/server.py
+++ dota_stats/server/server.py
@@ -16,37 +16,15 @@ app.config["MONGO_URI"] = os.environ["DOTA_MONGO_URI"]
 mongo = PyMongo(app)
 
 
-def get_health_chart(days, timezone, hour=True):
-    """Fetches a statistics health summary table and formats into a plotly
-    chart."""
-
-    df_summary, rows = fetch_summary.get_health_summary(mongo.db, days,
-                                                        timezone, hour)
-
-    # For plot
-    fig = go.Figure(data=[
-        go.Bar(name='Normal',
-               x=df_summary.index.values,
-               y=df_summary['normal']),
-        go.Bar(name='High',
-               x=df_summary.index.values,
-               y=df_summary['high']),
-        go.Bar(name='Very High',
-               x=df_summary.index.values,
-               y=df_summary['very_high']),
-    ])
-    fig.update_layout(barmode='stack')
-    record_count_plot = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return record_count_plot, rows
-
-
 @app.route('/')
-def status():
-    """Index page for server, currently contains everything on the site"""
+def root():
+    """Landing page..."""
+    return render_template("index.html")
 
-    print("Start of Win Rate/Pick Rate")
 
+@app.route('/win-rate')
+def win_rate():
+    """Win rate versus pick rate charts"""
     # Win rate / pick rate by skill level
     df_sql = win_rate_pick_rate.get_current_win_rate_table(mongo.db, 3)
 
@@ -81,25 +59,56 @@ def status():
     win_rate_2 = json.dumps(pick_vs_win[2], cls=plotly.utils.PlotlyJSONEncoder)
     win_rate_3 = json.dumps(pick_vs_win[3], cls=plotly.utils.PlotlyJSONEncoder)
 
-    print("Start of Health Metrics")
-
-    # ---------------------------------------------------------------
-    # Health metrics
-    # ---------------------------------------------------------------
-    rec_plot30, _ = get_health_chart(30, 'US/Eastern', hour=False)
-    rec_plot3, rec_count_table = get_health_chart(3, 'US/Eastern')
-
-
-    print("Done")
-
-    return render_template("index.html",
+    return render_template("winrate.html",
                            radiant_vs_dire=radiant_vs_dire,
                            win_rate_1=win_rate_1,
                            win_rate_2=win_rate_2,
                            win_rate_3=win_rate_3,
+                           )
+
+
+def create_status_fig(df_summary):
+    """Return plotly chart for a status report
+
+    @param df_summary: Pandas DataFrame from `fetch_summary`
+    @return: JSON for plotly
+    """
+    fig = go.Figure(data=[
+        go.Bar(name='Normal',
+               x=df_summary.index.values,
+               y=df_summary['normal']),
+        go.Bar(name='High',
+               x=df_summary.index.values,
+               y=df_summary['high']),
+        go.Bar(name='Very High',
+               x=df_summary.index.values,
+               y=df_summary['very_high']),
+    ])
+    fig.update_layout(barmode='stack')
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+@app.route('/status')
+def status():
+    """Fetches a statistics health summary table and formats into a plotly
+    chart."""
+
+    df_short = fetch_summary.get_health_summary(mongo.db, days=3, hour=True)
+    rec_plot3 = create_status_fig(df_short)
+
+    # Convert to list of tuples for tabular view...
+    rec_count_table = []
+    for idx, row in df_short.iterrows():
+        rec_count_table.append((idx, row[0], row[1], row[2]))
+
+    df_long = fetch_summary.get_health_summary(mongo.db, days=30, hour=False)
+    rec_plot30 = create_status_fig(df_long)
+
+    return render_template("status.html",
                            rec_count_table=rec_count_table,
                            rec_plot3=rec_plot3,
                            rec_plot30=rec_plot30, )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
